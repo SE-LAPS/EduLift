7d4c4d00af18e326fd41713187011e06d9d8de11
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2024-09-16 23:33:15 +0530

    Delete main.c

--- src/main.c
+++
@@ -1,8 +0,0 @@
-#include "control_app.h"
-#include "identification_app.h"
-#include "step_response_app.h"
-
-#define APP_TARGET \
-    runControlApp  // runStepResponseApp // runIdentificationApp
-
-int main() { APP_TARGET(); }
