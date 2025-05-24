53192f4813670ba38cd48d0edafaa1dc207d5354
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-05-24 11:29:18 +0530

    Create __init__.py

---
+++ backend/models/__init__.py
@@ -0,0 +1,6 @@
+from models.user import User
+from models.test import Test, TestQuestion, TestQuestionOption, StudentTest, StudentAnswer
+from models.exam import Exam, StudentExam, ExamEvaluation
+from models.handwriting import HandwritingSample, HandwritingModel
+
+# This file imports all models to make them available when importing from the models package
