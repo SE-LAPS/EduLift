af57bb06d42ddc64aaee8a4be2f0c9c99642fa77
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-06-11 17:40:53 +0530

    Create 500.tsx

---
+++ frontend/src/pages/500.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Box, Typography, Container, Button, Paper } from '@mui/material';
+
+export default function Custom500() {
+  return (
+    <Container maxWidth="md">
+      <Box sx={{ my: 8, textAlign: 'center' }}>
+        <Paper elevation={3} sx={{ p: 4 }}>
+          <Typography variant="h2" component="h1" gutterBottom>
+            500
+          </Typography>
+          <Typography variant="h5" component="h2" gutterBottom>
+            Server Error
+          </Typography>
+          <Typography variant="body1" paragraph>
+            We're sorry, something went wrong on our server. Please try again later.
+          </Typography>
+          <Button variant="contained" color="primary" href="/">
+            Return to Homepage
+          </Button>
+        </Paper>
+      </Box>
+    </Container>
+  );
+} 
