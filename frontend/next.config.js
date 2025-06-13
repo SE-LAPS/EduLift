19c5a68816a67ba7e92aec771c28546cbd7f5afd
Lahiru Senavirathna <lahiruprasangika2018@gmail.com>
2025-06-13 17:35:21 +0530

    Update next.config.js

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -8,6 +8,14 @@ const nextConfig = {
   env: {
     API_URL: process.env.API_URL || 'http://localhost:5000/api',
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:5000/api/:path*', // Proxy to Backend
+      },
+    ];
+  },
 }
 
 module.exports = nextConfig
